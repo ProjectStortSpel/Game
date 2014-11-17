900f2add4e5fb0eeffb359367346c2c3faec5c8a
Erik <nil_erik@hotmail.com>
2014-11-17 14:37:27 +0100

    fix?

--- Source/Game/Game.cpp
+++ Source/Game/Game.cpp
@@ -1,12 +1,12 @@
 #include <SDL/SDL.h>
-#include <Renderer/GraphicDevice.h>
+//#include <Renderer/GraphicDevice.h>
 
 
 int main(int argc, char** argv)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	Renderer::GraphicDevice* gdevice = new Renderer::GraphicDevice();
+	/*Renderer::GraphicDevice* gdevice = new Renderer::GraphicDevice();
 	gdevice->Init();
 
 	while (true)
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
 			}
 		}
 		gdevice->Render();
-	}
+	}*/
 
 	SDL_Quit();
 	return 0;
