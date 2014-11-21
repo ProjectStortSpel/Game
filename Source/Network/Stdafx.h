c4d3057f47de48523cabe7273999f89cd0a800d1
Pontus Nilsson <pontaren_nilsson@hotmail.com>
2014-11-21 15:18:12 +0100

    Memory leak detection

--- Source/Network/Stdafx.h
+++ Source/Network/Stdafx.h
@@ -16,4 +16,19 @@ typedef std::function<void(unsigned char, NetConnection*)> NetEvent;
 	#define NetSleep(x) usleep(30 * 1000);
 #endif
 
+#ifdef _WIN32
+	#define _CRTDBG_MAP_ALLOC
+
+	#ifdef _DEBUG
+		#ifndef DBG_NEW
+			#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+			#define new DBG_NEW
+		#endif
+	#endif  // _DEBUG
+
+
+	#include <stdlib.h>
+	#include <crtdbg.h>
+#endif
+
 #endif
