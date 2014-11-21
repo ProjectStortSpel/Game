52ff649becf4261bc1b000570b2e3cb5b532ff12
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-11-21 15:14:57 +0100

    Window and Input

--- Source/Game/Game.cpp
+++ Source/Game/Game.cpp
@@ -1,6 +1,6 @@
 #include <SDL/SDL.h>
 #include "ECSL/ECSL.h"
-
+#include "Input/InputWrapper.h"
 #include "Renderer/GraphicDevice.h"
 
 int main(int argc, char** argv)
@@ -8,12 +8,26 @@ int main(int argc, char** argv)
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	Renderer::GraphicDevice* gd = new Renderer::GraphicDevice();
-
+	Input::InputWrapper INPUT = Input::InputWrapper::GetInstance();
 	gd->Init();
 
-	while (true)
+	bool lol = true;
+	while (lol)
+	{
 		gd->Render();
+		INPUT.Update();
+		
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			INPUT.PollEvent(e);
+		}
 
+		if (INPUT.GetKeyboard()->GetKeyState(SDL_SCANCODE_SPACE) == Input::InputState::PRESSED)
+			lol = false;
+	}
+		
+	delete gd;
 	SDL_Quit();
 	return 0;
 }
