72bdbee7bc062a9ce49de4067862788f389846a7
Erik <nil_erik@hotmail.com>
2014-11-25 14:43:49 +0100

    interface change

--- Source/Network/WinSocket.cpp
+++ Source/Network/WinSocket.cpp
@@ -1,9 +1,12 @@
 #ifdef _WIN32
 #include "Network/WinSocket.h"
+#include <WS2tcpip.h>
 
 WinSocket::WinSocket(int _domain, int _type, int _protocol)
 {
 	m_socket = socket(_domain, _type, _protocol);
+	m_remoteIP = "";
+	m_remotePort = 0;
 }
 
 WinSocket::~WinSocket()
@@ -11,19 +14,67 @@ WinSocket::~WinSocket()
 
 }
 
-
-bool WinSocket::Connect(void* _sockaddr, int* _addrlen)
-{ 
-	return false; 
+bool WinSocket::Initialize()
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		std::printf("Failed. Error Code : %d", WSAGetLastError());
+		return false;
+	}
+	return true;
 }
 
-bool WinSocket::Bind(void* _sockaddr, int* _addrlen)
+
+bool WinSocket::Connect(const char* _ip, const int _port)
 { 
-	return false; 
+	addrinfo hints = { 0 };
+	hints.ai_flags = AI_NUMERICHOST;
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	addrinfo *addrs = NULL;
+	if (getaddrinfo(_ip, NULL, &hints, &addrs) != 0)
+	{
+		return false;
+	}
+
+	sockaddr_in address;
+	address.sin_addr.S_un.S_addr = ((sockaddr_in*)(addrs->ai_addr))->sin_addr.s_addr;
+	address.sin_port = htons(_port);
+	address.sin_family = AF_INET;
+
+	freeaddrinfo(addrs);
+
+	if (connect(m_socket, (sockaddr*)&address, sizeof(address)) != 0)
+	{
+		m_remoteIP = _ip;
+		m_remotePort = _port;
+		return false;
+	}
+	return true; 
 }
 
-ISocket* WinSocket::Accept(void* _sockaddr, int* _addrlen)
+bool WinSocket::Bind(const int _port)
 { 
+	sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(_port);
+	address.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	if (bind(m_socket, (sockaddr*)&address, sizeof(address)) != 0)
+	{
+		return false;
+	}
+	return true;
+}
+
+ISocket* WinSocket::Accept()
+{ 
+	sockaddr incomingAddress;
+	int incomingAddressLength;
+	accept(m_socket, &incomingAddress, &incomingAddressLength);
+
 	return 0; 
 }
 
