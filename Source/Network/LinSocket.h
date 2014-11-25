a2abf2b4d33c766363def2b28a0260edb9e33736
Erik <nil_erik@hotmail.com>
2014-11-25 15:50:35 +0100

    lin test 2

--- Source/Network/LinSocket.h
+++ Source/Network/LinSocket.h
@@ -4,6 +4,16 @@
 #define LINSOCKET_H
 #include <sys/socket.h>
 
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+
 #include "Network/ISocket.h"
 
 
