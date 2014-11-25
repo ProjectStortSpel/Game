116ad4de5519d40173f7174f025aa15a0577ef93
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-11-25 17:15:24 +0100

    compute shader read and write texture

--- Externals/content/shaders/debuggText.glsl
+++ Externals/content/shaders/debuggText.glsl
@@ -1,27 +1,28 @@
 #version 430 core
-#extension GL_EXT_texture_array : enable
-#extension GL_NV_texture_array : enable
 
 // UNIFORMS
-layout (local_size_x = 8, local_size_y = 16) in;
+layout (local_size_x = 16, local_size_y = 16) in;
+
+//read texture 
+layout (rgba8, binding = 0) uniform image2D input_image;
 
 // output 
-layout (rgba32f, binding = 0) uniform image2D output_image;
+layout (rgba32f, binding = 1) uniform image2D output_image;
 
-layout (std430, binding = 1) buffer debugtext { int letter []; };
 
 void main(void)
 {
-	uint x = gl_WorkGroupID.x;
-	uint y = (gl_NumWorkGroups.y-1)-gl_WorkGroupID.y;
+	vec4 outData = imageLoad(input_image, ivec2(gl_GlobalInvocationID.xy));
+	//outData = vec4(1.0, 0.2, 0.2, 1.0);
 
-	uint xy = x + y * gl_NumWorkGroups.x;
-
-	vec4 something = vec4(0,float(letter[xy].x)/4608.f * 0.5,0,0);
-
+	//imageStore(
+	//	output_image,
+	//	ivec2(gl_GlobalInvocationID.xy),
+	//	something
+	//);
 	imageStore(
 		output_image,
 		ivec2(gl_GlobalInvocationID.xy),
-		something
+		outData
 	);
 }
