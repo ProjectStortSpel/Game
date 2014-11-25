ed85ec3b436e9743e20d24e751fcdbd142c99bc7
Erik <nil_erik@hotmail.com>
2014-11-25 15:54:02 +0100

    lin test 4

--- Source/Network/LinSocket.h
+++ Source/Network/LinSocket.h
@@ -7,9 +7,6 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h> 
