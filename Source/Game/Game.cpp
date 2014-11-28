469c996cfb5dbcc6f00233b3f82b1ca16daf2755
Johan Anderdahl <anders.kruger@hotmail.com>
2014-11-28 15:15:39 +0100

    no mem leaks in renderer

--- Source/Game/Game.cpp
+++ Source/Game/Game.cpp
@@ -65,8 +65,8 @@ int main(int argc, char** argv)
 
 		INPUT.Update();
 		RENDERER.Update(dt);
-		//gd->RenderSimpleText("This text render from GAME! \nThe x and y values in the function isn't pixel \ncoordinates, it's char position. Every char is \n8x16 pixels in size. Use \\n to change line.\n\n  !Not all chars is supported!\n\nRight now it clear the whole output image as well (Tell me when to remove this).", 10, 2);
-		//
+		RENDERER.RenderSimpleText("This text render from GAME! \nThe x and y values in the function isn't pixel \ncoordinates, it's char position. Every char is \n8x16 pixels in size. Use \\n to change line.\n\n  !Not all chars is supported!\n\nRight now it clear the whole output image as well (Tell me when to remove this).", 10, 2);
+		
 
 		RENDERER.Render();
 		
