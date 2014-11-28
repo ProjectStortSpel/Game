e03422dc8fdd661b0c1e45e4c86b5b0f08327e4a
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-11-28 09:20:07 +0100

    Windows fix

--- Source/ECSL/Managers/ComponentTypeManager.h
+++ Source/ECSL/Managers/ComponentTypeManager.h
@@ -43,7 +43,7 @@ namespace ECSL
 	template<typename ComponentType>
 	void ComponentTypeManager::AddComponentType()
 	{
-		TypeId componentId = ComponentManager::GetTableId<ComponentType>();
+		unsigned int componentId = ComponentManager::GetTableId<ComponentType>();
 
 		auto it = m_loadedComponentTypes->find(componentId);
 		if (it == m_loadedComponentTypes->end())
@@ -53,7 +53,7 @@ namespace ECSL
 	template<typename ComponentType>
 	unsigned int ComponentTypeManager::GetTableId()
 	{
-		static TypeId m_id = ++m_nextTableId;
+		static unsigned int m_id = ++m_nextTableId;
 		return m_id;
 	}
 }
