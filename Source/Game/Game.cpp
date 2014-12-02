34dc0b95fbe4dc2cea5c66d26b708953ba18220a
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-12-02 14:09:24 +0100

    Mergeeeee

--- Source/Game/Game.cpp
+++ Source/Game/Game.cpp
@@ -1,9 +1,9 @@
 #include <SDL/SDL.h>
-#include <SDL/SDL_thread.h>
 #include <ECSL/ECSL.h>
-#include <vector>
-#include <algorithm>
-#include <time.h>
+#include "Input/InputWrapper.h"
+#include "Renderer/GraphicDevice.h"
+#include "Timer.h"
+
 #ifdef WIN32
 	#define _CRTDBG_MAP_ALLOC
 	#include <stdlib.h>
@@ -11,6 +11,8 @@
 #endif
 
 using namespace ECSL;
+
+mat4 mat[1000];
 
 class TestSystem : public ECSL::System
 {
@@ -108,20 +110,127 @@ void lol()
 	delete(world);
 	delete(&ComponentTypeManager::GetInstance());
 }
+void LoadAlotOfBoxes(Renderer::GraphicDevice* r)
+{
+	// ADDING TEMP OBJECTS
+	for (int x = 0; x < 10; x++)
+	{
+		for (int y = 0; y < 10; y++)
+		{
+			mat[x + y * 10] = glm::translate(vec3(x - 5, -1, y - 5));
+			r->LoadModel("content/models/cube/", "cube.object", &mat[x + y * 10]);
+		}
+	}
+}
+void Start()
+{
+	/*	Initialize Renderer and Input	*/
+	Renderer::GraphicDevice RENDERER = Renderer::GraphicDevice();
+	RENDERER.Init();
+
+	Input::InputWrapper* INPUT = &Input::InputWrapper::GetInstance();
+	
+	LoadAlotOfBoxes(&RENDERER);
+	mat[100] = glm::translate(vec3(0, 0, 0));
+	int modelid = RENDERER.LoadModel("content/models/cube/", "cube.object", &mat[100]); // LOADMODEL RETURNS THE MODELID
+	RENDERER.ChangeModelTexture(modelid, "content/models/cube/NM_tst.png"); // CHANGING TEXTURE ON MODELID
+
+	bool lol = true;
+	float cd = 1.0f;
+	Timer timer;
+	while (lol)
+	{
+		// DT COUNTER
+		float dt = timer.ElapsedTimeInSeconds();
+		timer.Reset();
+
+		INPUT->Update();
+		RENDERER.Update(dt);
+		RENDERER.RenderSimpleText("This text render from GAME! \nThe x and y values in the function isn't pixel \ncoordinates, it's char position. Every char is \n8x16 pixels in size. Use \\n to change line.\n\n  !Not all chars is supported!\n\nRight now it clear the whole output image as well (Tell me when to remove this).", 10, 2);
+
+
+		RENDERER.Render();
+		INPUT->Update();
+
+
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+			case SDL_WINDOWEVENT:
+				RENDERER.PollEvent(e);
+				break;
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+			case SDL_FINGERMOTION:
+			case SDL_FINGERDOWN:
+			case SDL_FINGERUP:
+			case SDL_JOYAXISMOTION:
+			case SDL_JOYBALLMOTION:
+			case SDL_JOYHATMOTION:
+			case SDL_JOYBUTTONDOWN:
+			case SDL_JOYBUTTONUP:
+			case SDL_MOUSEMOTION:
+			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONUP:
+			case SDL_MOUSEWHEEL:
+			case SDL_MULTIGESTURE:
+				INPUT->PollEvent(e);
+				break;
+			}
+		}
+
+		// MOVE CUBE
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_UP) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(0, 0, -0.01f));
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_DOWN) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(0, 0, 0.01f));
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_LEFT) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(-0.01f, 0, 0));
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_RIGHT) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(0.01f, 0, 0));
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_SPACE) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(0, 0.01f, 0));
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_LSHIFT) == Input::InputState::DOWN)
+			mat[100] *= glm::translate(vec3(0, -0.01f, 0));
+
+		// MOVE CAMERA
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_W) == Input::InputState::DOWN)
+			RENDERER.GetCamera()->MoveForward(dt);
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_S) == Input::InputState::DOWN)
+			RENDERER.GetCamera()->MoveBackward(dt);
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_A) == Input::InputState::DOWN)
+			RENDERER.GetCamera()->MoveLeft(dt);
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_D) == Input::InputState::DOWN)
+			RENDERER.GetCamera()->MoveRight(dt);
+
+		// ROTATE CAMERA
+		if (INPUT->GetMouse()->GetButtonState(Input::LeftButton) == Input::InputState::DOWN)
+		{
+			int sizeX, sizeY;
+			RENDERER.GetWindowSize(sizeX, sizeY);
+
+			RENDERER.GetCamera()->UpdateMouse(sizeX*0.5, sizeY*0.5, INPUT->GetMouse()->GetX(), INPUT->GetMouse()->GetY());
+			INPUT->GetMouse()->SetPosition(sizeX*0.5, sizeY*0.5);
+			INPUT->GetMouse()->HideCursor(true);
+		}
+		else
+			INPUT->GetMouse()->HideCursor(false);
+		//-----------------------------------------------------------------------------------------------
+
+		if (INPUT->GetKeyboard()->GetKeyState(SDL_SCANCODE_ESCAPE) == Input::InputState::PRESSED)
+		{
+			lol = false;
+		}
+	}
+
+	delete(INPUT);
+}
 
 int main(int argc, char** argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	lol();
-	mutex = SDL_CreateMutex();
-	//if (!mutex)
-	//	printf("Mutex...");
-	//thread1 = SDL_CreateThread((SDL_ThreadFunction)PerfTest, "PerfTest", (void*)0);
-	//SDL_WaitThread(thread1, 0);
-	PerfTest(0);
-	SDL_DestroyMutex(mutex);
-	SDL_Quit();
-	system("pause");
+	Start();
 	#ifdef WIN32
 	_CrtDumpMemoryLeaks();
 	#endif
