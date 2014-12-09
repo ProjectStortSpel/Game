e010ccaf81b81270cfc6941d6b33ab7ac7f5b26e
Chris <Carl.Sebastian.Rapp@gmail.com>
2014-12-09 15:45:09 +0100

    instancing added.

--- Externals/content/shaders/VSDeferredPass1.glsl
+++ Externals/content/shaders/VSDeferredPass1.glsl
@@ -4,14 +4,18 @@ layout( location = 1 ) in vec3 VertexNormal;
 layout( location = 2 ) in vec3 VertexTangent;
 layout( location = 3 ) in vec3 VertexBiTangent;
 layout( location = 4 ) in vec2 VertexTexCoord;
+layout( location = 5 ) in mat4 ModelViewMatrix;
+// used locations 5-8
+layout( location = 9 ) in mat3 NormalMatrix;
+// used locations 9-12
+
+//flat out int instanceID;
 
 out vec3 Normal;
 out vec3 Tan;
 out vec3 BiTan;
 out vec2 TexCoord;
 
-uniform mat4 ModelViewMatrix;
-uniform mat3 NormalMatrix;
 uniform mat4 ProjectionMatrix;
 
 void main()
@@ -22,4 +26,6 @@ void main()
 	TexCoord = VertexTexCoord;
 	mat4 MVP = ProjectionMatrix * ModelViewMatrix;
 	gl_Position = MVP * vec4(VertexPosition, 1.0);
+
+	//instanceID = gl_InstanceID;
 }
