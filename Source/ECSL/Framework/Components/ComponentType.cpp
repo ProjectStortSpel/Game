b3470f4aff904065f9b1b896815d9762e76b1d7a
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-12-10 09:00:57 +0100

    Added Offset to Data Type

--- Source/ECSL/Framework/Components/ComponentType.cpp
+++ Source/ECSL/Framework/Components/ComponentType.cpp
@@ -4,8 +4,9 @@ using namespace ECSL;
 
 ComponentType::ComponentType(std::string _name,
 		TableType _tableType,
-		std::map<std::string, ComponentVariable>& _variables)
-		: m_name(_name), m_tableType(_tableType), m_variables(_variables), m_byteSize(0)
+		std::map<std::string, ComponentVariable>& _variables,
+		std::map<unsigned int, ComponentDataType>& _offsetToTypes)
+		: m_name(_name), m_tableType(_tableType), m_variables(_variables), m_byteSize(0), m_offsetToType(_offsetToTypes)
 {
 	/* Calculate total byte size for component type */
 	for (auto it = m_variables.begin(); it != m_variables.end(); ++it)
