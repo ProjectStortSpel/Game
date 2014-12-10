58cde97dbb6305c184944edd531d9ead03a06f37
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-12-10 10:14:53 +0100

    ops

--- Source/Game/NewGame.cpp
+++ Source/Game/NewGame.cpp
@@ -4,18 +4,8 @@
 	#include <VLD/vld.h>
 #endif
 
-
-#include <glm/common.hpp>
-#include "ECSL/Framework/Parsing/EntityTemplateReader.h"
-#include "ECSL/Framework/Common/FileHelper.h"
-
-
 int main(int argc, char** argv)
 {
-
-
-
-
 	GameCreator* newGame = new GameCreator();
 
 	newGame->InitializeLua();
