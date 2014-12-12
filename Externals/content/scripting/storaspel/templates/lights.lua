4b98005cad113a2485026a3846e697eeebc47d63
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2014-12-12 15:58:15 +0100

    hehe

--- Externals/content/scripting/storaspel/templates/lights.lua
+++ Externals/content/scripting/storaspel/templates/lights.lua
@@ -2,5 +2,5 @@
 local PointlightRed = EntityTemplate()
 PointlightRed.Name = "Pointlight"
 PointlightRed:SetPointLight("Pointlight", 7.0, 4.0, 7.0, 1.0, 0.8, 0.8, 0.8, 0.2, 0.2, 10.0)
-PointlightRed.SetFloat("SyncNetwork", 0.0f)
+PointlightRed:AddComponent("SyncNetwork")
 EntityTemplateManager:AddTemplate(PointlightRed)
