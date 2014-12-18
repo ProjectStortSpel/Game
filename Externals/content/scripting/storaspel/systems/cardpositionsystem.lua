8431c92bd57e1f2e41f4ca3dd9d7fa271852678c
Johan Anderdahl <anders.kruger@hotmail.com>
2014-12-18 17:18:45 +0100

    set card model

--- Externals/content/scripting/storaspel/systems/cardpositionsystem.lua
+++ Externals/content/scripting/storaspel/systems/cardpositionsystem.lua
@@ -18,3 +18,22 @@ CardPositionSystem.Initialize = function(self)
 	
 	print("Card position System initialized!")
 end
+
+CardPositionSystem.OnEntityAdded = function(self, entityId)
+
+	local model = self:GetComponent(entityId, "Model", 0)
+	local action = self:GetComponent(entityId, "CardAction", 0)
+	if action == "Forward" then
+		model:SetModel("forward", "cards")
+	elseif action == "Backward" then
+		model:SetModel("back", "cards")
+	elseif action == "TurnRight" then
+		model:SetModel("turnright", "cards")
+	elseif action == "TurnLeft" then
+		model:SetModel("turnleft", "cards")
+	elseif action == "TurnAround" then
+		model:SetModel("turnaround", "cards")
+	else
+		model:SetModel("dodge", "cards")
+	end
+end
