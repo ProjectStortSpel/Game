5ab7fed62f685faf5feca79795b71bbe24189945
Erik Nilsson <nil_erik@hotmail.com>
2014-12-20 21:13:31 +0100

    int ExecuteCommand();
    	int AddToCommandQueue();
    	int AddCommand();
    
    	int RemoveCommand();
    	int ClearCommands();
    	int ClearHistory();

--- Source/Game/LuaBridge/Console/LuaConsole.cpp
+++ Source/Game/LuaBridge/Console/LuaConsole.cpp
@@ -1,6 +1,7 @@
 #include "LuaConsole.h"
 #include "LuaEmbedder/LuaEmbedder.h"
 #include "Console/Console.h"
+#include <algorithm>
 
 namespace LuaBridge
 {
@@ -8,11 +9,28 @@ namespace LuaBridge
   {
     int Print();
 	int IsOpen();
+	int ExecuteCommand();
+	int AddToCommandQueue();
+	int AddCommand();
+
+	int RemoveCommand();
+	int ClearCommands();
+	int ClearHistory();
 
     void Embed()
     {
       LuaEmbedder::AddFunction("Print", &Print, "Console");
 	  LuaEmbedder::AddFunction("IsOpen", &IsOpen, "Console");
+
+	  LuaEmbedder::AddFunction("ExecuteCommand", &ExecuteCommand, "Console");
+	  LuaEmbedder::AddFunction("AddToCommandQueue", &AddToCommandQueue, "Console");
+
+	  LuaEmbedder::AddFunction("AddCommand", &AddCommand, "Console");
+
+	  LuaEmbedder::AddFunction("RemoveCommand", &RemoveCommand, "Console");
+	  LuaEmbedder::AddFunction("ClearCommands", &ClearCommands, "Console");
+	  LuaEmbedder::AddFunction("ClearHistory", &ClearHistory, "Console");
+
     }
     
     int Print()
@@ -29,6 +47,61 @@ namespace LuaBridge
 		return 1;
 	}
 
+	int ExecuteCommand()
+	{
+		Console::ConsoleManager::GetInstance().ExecuteCommand(LuaEmbedder::PullString(1).c_str());
+		return 0;
+	}
 
+	int AddToCommandQueue()
+	{
+		Console::ConsoleManager::GetInstance().AddToCommandQueue(LuaEmbedder::PullString(1).c_str());
+		return 0;
+	}
+
+	void LuaConsoleFunction(std::string _command, std::vector<Console::Argument>* _args)
+	{
+		LuaEmbedder::PushString(_command);
+
+		for (int i = 0; i < _args->size(); ++i)
+		{
+			if (_args->at(i).ArgType == Console::ArgumentType::Text)
+				LuaEmbedder::PushString(_args->at(i).Text);
+			else if (_args->at(i).ArgType == Console::ArgumentType::Number)
+				LuaEmbedder::PushFloat(_args->at(i).Number);
+		}
+
+		LuaEmbedder::CallSavedFunction(_command, _args->size() + 1);
+	}
+
+	int AddCommand()
+	{
+		std::string command = LuaEmbedder::PullString(1);
+		std::transform(command.begin(), command.end(), command.begin(), ::tolower);
+
+		LuaEmbedder::SaveFunction(2, command);
+
+		Console::ConsoleManager::GetInstance().AddCommand(command.c_str(), &LuaConsoleFunction);
+
+		return 0;
+	}
+
+	int RemoveCommand()
+	{
+		Console::ConsoleManager::GetInstance().RemoveCommand(LuaEmbedder::PullString(1).c_str());
+		return 0;
+	}
+
+	int ClearCommands()
+	{
+		Console::ConsoleManager::GetInstance().ClearCommands();
+		return 0;
+	}
+
+	int ClearHistory()
+	{
+		Console::ConsoleManager::GetInstance().ClearHistory();
+		return 0;
+	}
   }
 }
