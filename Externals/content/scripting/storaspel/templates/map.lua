a76f461e419451527eec993303e232895369dcf6
Erik Nilsson <nil_erik@hotmail.com>
2014-12-20 21:14:55 +0100

    Added Card component to Cards

--- Externals/content/scripting/storaspel/templates/map.lua
+++ Externals/content/scripting/storaspel/templates/map.lua
@@ -9,6 +9,7 @@ EntityTemplateManager:AddTemplate(Tile)
 
 local Card = EntityTemplate()
 Card.Name = "Card"
+Card:AddComponent("Card")
 Card:AddComponent("CardPrio")
 Card:AddComponent("CardAction")
 EntityTemplateManager:AddTemplate(Card)
