50f74be1a214b87c78a1b6e3cb881a1a45bb6253
Erik Nilsson <nil_erik@hotmail.com>
2014-12-24 03:22:41 +0100

    Only add syncnetwork if the template doesn't have it

--- Source/Game/GameConsole.cpp
+++ Source/Game/GameConsole.cpp
@@ -31,7 +31,9 @@ void GameConsole::CreateObject(std::string _command, std::vector<Console::Argume
 
 	unsigned int mId = m_world->CreateNewEntity(_template);
 	m_world->CreateComponentAndAddTo("ChangedComponents", mId);
-	m_world->CreateComponentAndAddTo("SyncNetwork", mId);
+
+	if (!m_world->EntityHasComponent(mId, "SyncNetwork"))
+		m_world->CreateComponentAndAddTo("SyncNetwork", mId);
 
 	std::stringstream ss;
 	ss << "Entity with id #" << mId << " has been created!";
