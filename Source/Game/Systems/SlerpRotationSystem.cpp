f47f2f3af10ae837275fc6a1f7b22fb68977d41b
Johannnes Eriksson Wallin <jorre92@hotmail.com>
2015-01-26 17:26:29 +0100

    Opt. slerp rotaion and made a fail safe when added.

--- Source/Game/Systems/SlerpRotationSystem.cpp
+++ Source/Game/Systems/SlerpRotationSystem.cpp
@@ -19,30 +19,41 @@ void SlerpRotationSystem::Initialize()
 
 void SlerpRotationSystem::Update(float _dt)
 {
-	std::vector<unsigned int> entities = *GetEntities();
-	
-	for (int i = 0; i < entities.size(); i++)
+	if (NetworkInstance::GetServer()->IsRunning())
 	{
-		if (NetworkInstance::GetServer()->IsRunning())
+		std::vector<unsigned int> entities = *GetEntities();
+		for (int i = 0; i < entities.size(); i++)
 		{
+		
 			float* time_data = (float*)GetComponent(entities[i], "SlerpRotation", "time");
 			float* from_data = (float*)GetComponent(entities[i], "SlerpRotation", "fromW");
+			float* from_dataX = (float*)GetComponent(entities[i], "SlerpRotation", "fromX");
+			float* from_dataY = (float*)GetComponent(entities[i], "SlerpRotation", "fromY");
+			float* from_dataZ = (float*)GetComponent(entities[i], "SlerpRotation", "fromZ");
 			float* to_data = (float*)GetComponent(entities[i], "SlerpRotation", "toW");
+			float* to_dataX = (float*)GetComponent(entities[i], "SlerpRotation", "toX");
+			float* to_dataY = (float*)GetComponent(entities[i], "SlerpRotation", "toY");
+			float* to_dataZ = (float*)GetComponent(entities[i], "SlerpRotation", "toZ");
 
 			Quaternion q_from, q_to;
-
-			q_to.Rotate(glm::vec3(0, 1, 0), *to_data);
-
-			q_from.Rotate(glm::vec3(0, 1, 0), *from_data);
-
-			q_to = q_from*q_to;
-
 			Quaternion current_rotation;
+
+			q_from.m_w = *from_data;
+			q_from.m_x = *from_dataX;
+			q_from.m_y = *from_dataY;
+			q_from.m_z = *from_dataZ;
+
+			q_to.m_w = *to_data;
+			q_to.m_x = *to_dataX;
+			q_to.m_y = *to_dataY;
+			q_to.m_z = *to_dataZ;
+
+			
 
 			
 			glm::vec3 euler;
 
-			(*time_data) += _dt;
+			(*time_data) += _dt * 1.2f;
 
 			if ((*time_data) > 1)
 			{
@@ -72,10 +83,35 @@ void SlerpRotationSystem::OnEntityAdded(unsigned int _entityId)
 {
 	//printf("Adding\n");
 	unsigned int entity = _entityId;
-	
-	float* a = (float*)GetComponent(entity, "SlerpRotation", "time");
-	
-	(*a) = 0.0;
+
+	float* time_data = (float*)GetComponent(_entityId, "SlerpRotation", "time");
+	float* from_data = (float*)GetComponent(_entityId, "SlerpRotation", "fromW");
+	float* from_dataX = (float*)GetComponent(_entityId, "SlerpRotation", "fromX");
+	float* from_dataY = (float*)GetComponent(_entityId, "SlerpRotation", "fromY");
+	float* from_dataZ = (float*)GetComponent(_entityId, "SlerpRotation", "fromZ");
+	float* to_data = (float*)GetComponent(_entityId, "SlerpRotation", "toW");
+	float* to_dataX = (float*)GetComponent(_entityId, "SlerpRotation", "toX");
+	float* to_dataY = (float*)GetComponent(_entityId, "SlerpRotation", "toY");
+	float* to_dataZ = (float*)GetComponent(_entityId, "SlerpRotation", "toZ");
+
+	Quaternion q_from, q_to;
+
+	q_to.Rotate(glm::vec3(*to_dataX, *to_dataY, *to_dataZ), *to_data);
+
+	q_from.Rotate(glm::vec3(*from_dataX, *from_dataY, *from_dataZ), *from_data);
+
+	q_to = q_from*q_to;
+
+	*time_data = 0.0;
+	*from_data	= q_from.m_w;
+	*from_dataX	= q_from.m_x;
+	*from_dataY	= q_from.m_y;
+	*from_dataZ	= q_from.m_z;
+	*to_data 	= q_to.m_w;
+	*to_dataX	= q_to.m_x;
+	*to_dataY	= q_to.m_y;
+	*to_dataZ	= q_to.m_z;
+
 }
 
 void SlerpRotationSystem::OnEntityRemoved(unsigned int _entityId)
