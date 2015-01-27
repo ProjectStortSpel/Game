6983211f1a2bcaad7a2f26ad65e68958dbe470a9
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2015-01-27 16:38:58 +0100

    More Systems

--- Externals/content/scripting/storaspelthreaded/Systems/Client/InitClientSystems.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Client/InitClientSystems.lua
@@ -26,19 +26,10 @@ worldCreator:AddSystemToCurrentGroup(GameInterfaceSystem)
 
 --	Card Systems
 package.path = package.path .. ";../../../Externals/content/scripting/storaspelthreaded/Systems/Client/InterfaceSystems/?.lua"
-require "cl_RconMenuSystem"
-require "cl_OptionsMenuSystem"
-require "cl_HoverSizeSystem"
-require "cl_GameMenuSystem"
-require "cl_GameInterfaceSystem"
+require "cl_CardAddModelSystem"
+require "cl_CardHoverSystem"
 
 worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(PlayerIndicatorSystem)
+worldCreator:AddSystemToCurrentGroup(CardAddModelSystem)
 worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(OptionMenuSystem)
-worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(HoverSizeSystem)
-worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(GameMenuSystem)
-worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(GameInterfaceSystem)
+worldCreator:AddSystemToCurrentGroup(CardHoverSystem)
