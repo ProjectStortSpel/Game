1b4d9b8bebf6f237ff2da6194e9df55454bbbacd
TENFIRE <nil_erik@hotmail.com>
2015-01-28 23:05:40 +0100

    IOS Text fix

--- Source/AndroidRenderer/TextureLoader.h
+++ Source/AndroidRenderer/TextureLoader.h
@@ -61,6 +61,8 @@ static unsigned int LoadTexture(SDL_Surface* surface, GLenum textureSlot)
 	glBindTexture(GL_TEXTURE_2D, texHandle);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
 	return texHandle;
 }
