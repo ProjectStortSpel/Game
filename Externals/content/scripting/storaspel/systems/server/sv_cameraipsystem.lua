751530c44d4a66b53cd6848530a2caf8242fc79c
andoors <anders.kruger@hotmail.com>
2015-01-30 14:53:51 +0100

    fisk?

--- Externals/content/scripting/storaspel/systems/server/sv_cameraipsystem.lua
+++ Externals/content/scripting/storaspel/systems/server/sv_cameraipsystem.lua
@@ -11,23 +11,24 @@ CameraInterestPointSystem.OnEntityAdded = function(self, entityId)
 	if world:EntityHasComponent( entityId, "CameraOnPlayer") then
 		local players = self:GetEntities("Player")
 		for i = 1, #players do
-			local ip = world:GetComponent(players[i], "NetConnection", "IpAddress"):GetString()
-			local port = world:GetComponent(players[i], "NetConnection", "Port"):GetInt()
-			local unitId = world:GetComponent(players[i], "UnitEntityId", "Id"):GetInt()
-			local x, y, z = world:GetComponent(unitId, "Position", 0):GetFloat3(0)
-			local dx, dz = world:GetComponent(unitId, "Direction", 0):GetInt2(0)
-			
-			print(ry)
-			
-			-- CAMERA INTEREST POINT
-			local cipID = Net.StartPack("Client.SendCIP")
-			Net.WriteFloat(cipID, x)
-			Net.WriteFloat(cipID, z)
-			Net.WriteFloat(cipID, dx)
-			Net.WriteFloat(cipID, dz)
-			Net.WriteFloat(cipID, 1)
-			Net.Broadcast(cipID)
-			
+			if not world:EntityHasComponent( players[i], "IsSpectator") then
+				local ip = world:GetComponent(players[i], "NetConnection", "IpAddress"):GetString()
+				local port = world:GetComponent(players[i], "NetConnection", "Port"):GetInt()
+				local unitId = world:GetComponent(players[i], "UnitEntityId", "Id"):GetInt()
+				local x, y, z = world:GetComponent(unitId, "Position", 0):GetFloat3(0)
+				local dx, dz = world:GetComponent(unitId, "Direction", 0):GetInt2(0)
+				
+				print(ry)
+				
+				-- CAMERA INTEREST POINT
+				local cipID = Net.StartPack("Client.SendCIP")
+				Net.WriteFloat(cipID, x)
+				Net.WriteFloat(cipID, z)
+				Net.WriteFloat(cipID, dx)
+				Net.WriteFloat(cipID, dz)
+				Net.WriteFloat(cipID, 1)
+				Net.Broadcast(cipID)
+			end
 		end
 		world:KillEntity( entityId )
 	end
