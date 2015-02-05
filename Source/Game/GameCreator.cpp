dab4bc3fb94381b80e4711323d8da2ad7bc86f64
Marcus Svensson <marcussvensson92@live.se>
2015-02-05 13:38:27 +0100

    Bug fix: Clear graphical systems vector

--- Source/Game/GameCreator.cpp
+++ Source/Game/GameCreator.cpp
@@ -197,6 +197,7 @@ void GameCreator::InitializeWorld(std::string _gameMode)
 	//NetworkMessagesSystem* nms = new NetworkMessagesSystem();
 	//nms->SetConsole(&m_consoleManager);
 	
+	m_graphicalSystems.clear();
 	GraphicalSystem* graphicalSystem = 0;
 	graphicalSystem = new PointlightSystem(m_graphics);
 	m_graphicalSystems.push_back(graphicalSystem);
