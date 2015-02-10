71861016b6e07c69301a9f279040df7f77b76ada
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2015-02-10 12:12:43 +0100

    Pointlights added! WOOOO

--- Externals/content/scripting/storaspelthreaded/Systems/Server/InitServerSystems.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Server/InitServerSystems.lua
@@ -21,9 +21,12 @@ worldCreator:AddSystemToCurrentGroup(CheckpointSystem)
 --	Light Systems
 package.path = package.path .. ";../../../Externals/content/scripting/storaspelthreaded/Systems/Server/LightSystems/?.lua"
 require "sv_DirectionalLightSystem"
+require "sv_PointlightSystem"
 
 worldCreator:AddSystemGroup()
 worldCreator:AddSystemToCurrentGroup(DirectionalLightSystem)
+worldCreator:AddSystemGroup()
+worldCreator:AddSystemToCurrentGroup(PointlightSystem)
 
 --	Player Systems
 package.path = package.path .. ";../../../Externals/content/scripting/storaspelthreaded/Systems/Server/PlayerSystems/?.lua"
@@ -128,7 +131,7 @@ worldCreator:AddSystemToCurrentGroup(AbilityIWin)
 
 --	Spawn Systems
 package.path = package.path .. ";../../../Externals/content/scripting/storaspelthreaded/Systems/Server/SpawnSystems/?.lua"
-require "sv_SpawnSystem"
+require "sv_FindSpawnpointSystem"
 require "sv_GiveSpawnLocationSystem"
 require "sv_RespawnSystem"
 require "sv_PostSpawnSystem"
@@ -136,7 +139,7 @@ require "sv_PostSpawnSystem"
 worldCreator:AddSystemGroup()
 worldCreator:AddSystemToCurrentGroup(GiveSpawnLocationSystem)
 worldCreator:AddSystemGroup()
-worldCreator:AddSystemToCurrentGroup(SpawnSystem)
+worldCreator:AddSystemToCurrentGroup(FindSpawnpointSystem)
 worldCreator:AddSystemGroup()
 worldCreator:AddSystemToCurrentGroup(RespawnSystem)
 worldCreator:AddSystemGroup()
