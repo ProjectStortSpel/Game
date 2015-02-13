6053aa3ae82d86a7c9bb92ba4f6990c0e2b6a5e7
root <root@localhost.localdomain>
2015-02-13 14:39:17 +0100

    FileSystem works on Linux!

--- Source/FileSystem/Directory.cpp
+++ Source/FileSystem/Directory.cpp
@@ -2,18 +2,18 @@
 #include <cstring>
 
 #if defined(WIN32)
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include<windows.h>
-#elif defined(__IOS__) || defined(__OSX__)
-#include <sys/types.h>
-#include <sys/stat.h>
-#elif defined(__OSX__)
 
 #elif defined(__ANDROID__)
 
 #else
-//#include <sys/stat.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #endif
 
 #ifdef WIN32
@@ -41,14 +41,11 @@ namespace FileSystem
 		{
 			#if defined(WIN32)
 				return CreateDirectory(s2ws(_path).c_str(), NULL);
-			#elif defined(__IOS__) || defined(__OSX__)
-                return mkdir(_path.c_str(), 0775) == 0;
-			#elif defined(__OSX__)
 
 			#elif defined(__ANDROID__)
 
 			#else
-						//return mkdir(_path.c_str(), 0775) == 0;
+				return mkdir(_path.c_str(), 0775) == 0;
 			#endif
 			return false;
 		}
@@ -99,7 +96,7 @@ namespace FileSystem
 			if (_path.at(_path.size() - 1) == '/')
 				_path = _path.substr(0, _path.size() - 1);
 
-			#if defined(WIN32) || defined(__IOS__) || defined(__OSX__)
+			#if !defined(__ANDROID__)
 				struct stat info;
 				if (stat(_path.c_str(), &info) != 0)
 					return false;
