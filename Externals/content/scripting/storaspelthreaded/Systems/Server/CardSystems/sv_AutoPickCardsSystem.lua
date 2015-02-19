be97bc56a7bb5d57d00e62e0e91d7dc399569422
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2015-02-19 13:40:37 +0100

    Fixed typos/prints

--- Externals/content/scripting/storaspelthreaded/Systems/Server/CardSystems/sv_AutoPickCardsSystem.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Server/CardSystems/sv_AutoPickCardsSystem.lua
@@ -94,26 +94,20 @@ AutoPickCards.StealCardsFrom = function(self, playerIndex)
 	
 		local action = world:GetComponent(pickedCards[cardIndex], "CardAction", "Action"):GetText()
 		local prio = world:GetComponent(pickedCards[cardIndex], "CardPrio", "Prio"):GetInt()
-		print("CardID: " .. pickedCards[cardIndex])
-		print("Index: " .. cardIndex)
-		print("Action: " .. action)
-		print("Prio: " .. prio)
-	
-		world:RemoveComponentFrom("DealtCard", pickedCards[cardIndex])
 		
-		if world:EntityHasComponent(pickedCards[cardIndex], "ServerSelectedCard") then
-			world:RemoveComponentFrom("ServerSelectedCard", pickedCards[cardIndex])
-		end
-
+		world:RemoveComponentFrom("DealtCard", pickedCards[cardIndex])
 		world:CreateComponentAndAddTo("CardStep", pickedCards[cardIndex])
 		world:GetComponent(pickedCards[cardIndex], "CardStep", "Step"):SetInt(cardIndex)
 		world:GetComponent(pickedCards[cardIndex], "CardStep", "UnitEntityId"):SetInt(playerUnit)
+		
 		Net.SendEntityKill(pickedCards[cardIndex], playerIp, playerPort)
 	end
 	
 	--	Update unit status
 	world:CreateComponentAndAddTo("UnitSelectedCards", playerUnit)
 end
+
+
 
 AutoPickCards.EntitiesAdded = function(self, dt, taskIndex, taskCount, entities)
 
@@ -128,6 +122,8 @@ AutoPickCards.EntitiesAdded = function(self, dt, taskIndex, taskCount, entities)
 			for tPlayer = 1, #nonReadyPlayers do
 				self:StealCardsFrom(nonReadyPlayers[tPlayer])
 			end
+			
+			
 			
 			local id = world:CreateNewEntity()
 			world:CreateComponentAndAddTo("NotifyStartNewRound", id)
