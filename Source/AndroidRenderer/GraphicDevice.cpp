5840f53623456365f8d91392087bef68b00a1c7d
Chris <c.ljungberg90@gmail.com>
2015-02-19 11:01:25 +0100

    Android texture errors gone

--- Source/AndroidRenderer/GraphicDevice.cpp
+++ Source/AndroidRenderer/GraphicDevice.cpp
@@ -386,14 +386,16 @@ void GraphicDevice::BufferModel(int _modelId, ModelToLoad* _modelToLoad)
 	GLuint texture = AddTexture(obj.text, GL_TEXTURE1);
 	shaderPtr->CheckUniformLocation("diffuseTex", 1);
 
+	GLuint normal, specular;
+
 	if (_modelToLoad->RenderType != RENDER_INTERFACE)
 	{
 		// Import Normal map
-		GLuint normal = AddTexture(obj.norm, GL_TEXTURE2);
+		normal = AddTexture(obj.norm, GL_TEXTURE2);
 		shaderPtr->CheckUniformLocation("normalTex", 2);
 
 		// Import Specc Glow map
-		GLuint specular = AddTexture(obj.spec, GL_TEXTURE3);
+		specular = AddTexture(obj.spec, GL_TEXTURE3);
 		shaderPtr->CheckUniformLocation("specularTex", 3);
 	}
 
