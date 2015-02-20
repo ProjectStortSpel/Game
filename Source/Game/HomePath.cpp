1d695326b8b65fbe548a058004a563903f41d111
Erik Nilsson <nil_erik@hotmail.com>
2015-02-20 11:44:21 +0100

    You can now choose to specify a custom gamemode when geting paths. You now have to specify if you want the paths in "server order" or "client order".

--- Source/Game/HomePath.cpp
+++ Source/Game/HomePath.cpp
@@ -12,14 +12,27 @@ namespace HomePath
 
 	std::string GetHomePath()
 	{
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+		return "../../../Externals/content/";
+#endif
 		return "content/";
 	}
 
 	std::string GetGameModePath()
 	{
+		return GetGameModePath(gamemode);
+	}
+
+	std::string GetGameModePath(std::string _gamemode)
+	{
 		//std::string path = "content/gamemodes/";
+
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+		std::string path = "../../../Externals/content/scripting/";
+#else
 		std::string path = "content/scripting/";
-		path.append(gamemode);
+#endif
+		path.append(_gamemode);
 		path.append("/");
 		return path;
 	}
@@ -49,10 +62,15 @@ namespace HomePath
 
 	std::string GetSecondaryGameModePath()
 	{
+		return GetSecondaryGameModePath(gamemode);
+	}
+
+	std::string GetSecondaryGameModePath(std::string _gamemode)
+	{
 		std::string path = GetSecondaryHomePath();
 		//path.append("gamemodes/");
 		path.append("scripting/");
-		path.append(gamemode);
+		path.append(_gamemode);
 		path.append("/");
 		return path;
 	}
@@ -60,16 +78,22 @@ namespace HomePath
 	std::string GetDownloadHomePath()
 	{
 		std::string path = GetSecondaryHomePath();
+
 		path.append("downloads/");
 		return path;
 	}
 
 	std::string GetDownloadGameModePath()
 	{
-		std::string path = GetSecondaryHomePath();
+		return GetDownloadGameModePath(gamemode);
+	}
+
+	std::string GetDownloadGameModePath(std::string _gamemode)
+	{
+		std::string path = GetDownloadHomePath();
 		//path.append("downloads/gamemodes/");
-		path.append("downloads/scripting/");
-		path.append(gamemode);
+		path.append("scripting/");
+		path.append(_gamemode);
 		path.append("/");
 		return path;
 	}
@@ -79,38 +103,103 @@ namespace HomePath
 		gamemode = _gamemode;
 	}
 
-	std::vector<std::string> GetPaths()
+	std::vector<std::string> GetPaths(Type _type)
+	{
+		return GetPaths(_type, gamemode);
+	}
+
+	std::vector<std::string> GetPaths(Type _type, std::string _gamemode)
 	{
 		std::vector<std::string> paths;
-		//Gamemode paths
-		paths.push_back(GetGameModePath());
-		paths.push_back(GetSecondaryGameModePath());
-		paths.push_back(GetDownloadGameModePath());
+		if (_type == Server)
+		{
+			//Gamemode paths
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+			paths.push_back(GetGameModePath(_gamemode));
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+#else
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+			paths.push_back(GetGameModePath(_gamemode));
+#endif
+			paths.push_back(GetDownloadGameModePath(_gamemode));
 
-		//Homepaths
-		paths.push_back(GetHomePath());
-		paths.push_back(GetSecondaryHomePath());
-		paths.push_back(GetDownloadHomePath());
+			//Homepaths
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+			paths.push_back(GetHomePath());
+			paths.push_back(GetSecondaryHomePath());
+#else
+			paths.push_back(GetSecondaryHomePath());
+			paths.push_back(GetHomePath());
+#endif
+			paths.push_back(GetDownloadHomePath());
+		}
+		else if (_type == Client)
+		{
+			//Gamemode paths
+			paths.push_back(GetDownloadGameModePath(_gamemode));
+			paths.push_back(GetGameModePath(_gamemode));
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+
+			//Homepaths
+			paths.push_back(GetDownloadHomePath());
+			paths.push_back(GetHomePath());
+			paths.push_back(GetSecondaryHomePath());
+		}
 		return paths;
 	}
 
-	std::vector<std::string> GetGameModePaths()
+	std::vector<std::string> GetGameModePaths(Type _type)
+	{
+		return GetGameModePaths(_type, gamemode);
+	}
+
+	std::vector<std::string> GetGameModePaths(Type _type, std::string _gamemode)
 	{
 		std::vector<std::string> paths;
-		//Gamemode paths
-		paths.push_back(GetGameModePath());
-		paths.push_back(GetSecondaryGameModePath());
-		paths.push_back(GetDownloadGameModePath());
+		if (_type == Server)
+		{
+			//Gamemode paths
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+			paths.push_back(GetGameModePath(_gamemode));
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+#else
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+			paths.push_back(GetGameModePath(_gamemode));
+#endif
+			paths.push_back(GetDownloadGameModePath(_gamemode));
+		}
+		else if (_type == Client)
+		{
+			//Gamemode paths
+			paths.push_back(GetDownloadGameModePath(_gamemode));
+			paths.push_back(GetGameModePath(_gamemode));
+			paths.push_back(GetSecondaryGameModePath(_gamemode));
+		}
 		return paths;
 	}
 
-	std::vector<std::string> GetHomePaths()
+	std::vector<std::string> GetHomePaths(Type _type)
 	{
 		std::vector<std::string> paths;
-		//Homepaths
-		paths.push_back(GetHomePath());
-		paths.push_back(GetSecondaryHomePath());
-		paths.push_back(GetDownloadHomePath());
+		if (_type == Server)
+		{
+			//Homepaths
+#if defined(_DEBUG) && !defined(__ANDROID__) && !defined(__APPLE__)
+			paths.push_back(GetHomePath());
+			paths.push_back(GetSecondaryHomePath());
+#else
+			paths.push_back(GetSecondaryHomePath());
+			paths.push_back(GetHomePath());
+#endif
+			paths.push_back(GetDownloadHomePath());
+		}
+		else if (_type == Client)
+		{
+			//Homepaths
+			paths.push_back(GetDownloadHomePath());
+			paths.push_back(GetHomePath());
+			paths.push_back(GetSecondaryHomePath());
+		}
 		return paths;
 	}
 }
