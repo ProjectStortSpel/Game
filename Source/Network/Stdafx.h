b84488444a4911e77f3ab1a8c0ea8715a5928fbd
Pontus Nilsson <pontaren_nilsson@hotmail.com>
2015-02-24 15:31:13 +0100

    1

--- Source/Network/Stdafx.h
+++ Source/Network/Stdafx.h
@@ -29,6 +29,7 @@
 
 #define MAX_PACKET_SIZE 65535 // Max value for unsigned short
 #define SAFE_DELETE(x) if(x) { delete x; x = 0; }
+#define SAFE_DELETE_ARRAY(x) if(x) { delete []x; x = 0; }
 
 #define TYP_INIT 0 
 #define TYP_SMLE 1 
