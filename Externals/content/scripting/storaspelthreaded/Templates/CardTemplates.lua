6a40724a6ef6112dd3641e4d0ee1c17fd0de9cb0
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2015-02-25 10:51:41 +0100

    New Card Template

--- Externals/content/scripting/storaspelthreaded/Templates/CardTemplates.lua
+++ Externals/content/scripting/storaspelthreaded/Templates/CardTemplates.lua
@@ -4,3 +4,19 @@ Card:AddComponent("Card")
 Card:AddComponent("CardPrio")
 Card:AddComponent("CardAction")
 EntityTemplateManager:AddTemplate(Card)
+
+
+local CardAboveHead = EntityTemplate()
+CardAboveHead.Name = "CardAboveHead"
+
+	CardAboveHead:AddComponent("SyncNetwork")
+	CardAboveHead:AddComponent("WorldToViewSpace")
+	CardAboveHead:AddComponent("Parent")
+	CardAboveHead:AddComponent("Model")
+	CardAboveHead:AddComponent("CardAboveHead")
+	CardAboveHead:AddComponent("Position")
+	CardAboveHead:AddComponent("Rotation")
+	CardAboveHead:AddComponent("Scale")
+	CardAboveHead:AddComponent("Color")
+
+EntityTemplateManager:AddTemplate(CardAboveHead)
