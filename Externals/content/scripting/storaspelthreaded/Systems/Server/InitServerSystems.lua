8086f4e54043bebafec989379293534fa8218526
Carl Rapp <Carl.Sebastian.Rapp@gmail.com>
2015-02-25 10:52:44 +0100

    Added new system

--- Externals/content/scripting/storaspelthreaded/Systems/Server/InitServerSystems.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Server/InitServerSystems.lua
@@ -46,7 +46,7 @@ require "sv_DealCardsSystem"
 require "sv_TakeCardStepFromUnitSystem"
 require "sv_TakeCardsFromPlayerSystem"
 require "sv_PlayCardSystem"
-require "sv_PlaceCardAboveUnitSystem"
+--require "sv_PlaceCardAboveUnitSystem"
 require "sv_SortClientSelectedCards"
 require "sv_ClearServerSelectedSystem"
 
@@ -135,7 +135,7 @@ worldCreator:AddSystemToCurrentGroup(DealCardsSystem)
 worldCreator:AddSystemToCurrentGroup(TakeCardStepsFromUnitSystem)
 worldCreator:AddSystemToCurrentGroup(TakeCardsFromPlayerSystem)
 worldCreator:AddSystemToCurrentGroup(PlayCardSystem)
-worldCreator:AddSystemToCurrentGroup(PlaceCardAboveUnitSystem)
+--worldCreator:AddSystemToCurrentGroup(PlaceCardAboveUnitSystem)
 
 --worldCreator:AddSystemGroup() << TEMP REMOVE 3
 worldCreator:AddSystemToCurrentGroup(ActionMoveForwardSystem)
