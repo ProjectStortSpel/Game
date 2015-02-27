1823a1ccb921b186bc7b115addbdd65a3bf9e087
Marcus Svensson <marcussvensson92@live.se>
2015-02-27 12:33:18 +0100

    Merge

--- Externals/content/scripting/storaspelthreaded/Systems/Client/MiscSystems/cl_FlowingWaterSystem.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Client/MiscSystems/cl_FlowingWaterSystem.lua
@@ -35,22 +35,38 @@ FlowingWaterSystem.EntitiesAdded = function(self, dt, entities)
 	
 	for i = 1, #entities do
 		local	X, Z	=	world:GetComponent(entities[i], "MapPosition", "X"):GetInt2()
+		local 	modelname = world:GetComponent(entities[i], "Model", "ModelName"):GetText()
+		
 		local	newFlowingTile	=	world:CreateNewEntity("FlowingWater")
-		world:SetComponent(newFlowingTile, "Model", "ModelName", "waterplane");
-		world:SetComponent(newFlowingTile, "Model", "ModelPath", "water");
+		
+		--if modelname == "riverstraight" then
+			world:SetComponent(newFlowingTile, "Model", "ModelName", "waterplane");
+			world:SetComponent(newFlowingTile, "Model", "ModelPath", "water");
+			
+			world:GetComponent(newFlowingTile, "Position", "X"):SetFloat3(X, 0.40, Z)
+			
+		--elseif modelname == "rivercorner" then
+		--	world:SetComponent(newFlowingTile, "Model", "ModelName", "watercorner");
+		--	world:SetComponent(newFlowingTile, "Model", "ModelPath", "watercorner");
+			
+		--	world:GetComponent(newFlowingTile, "Position", "X"):SetFloat3(X, 0.40, Z)
+		--end
+		
 		world:SetComponent(newFlowingTile, "Model", "RenderType", 5);
 		
-		world:GetComponent(newFlowingTile, "Position", "X"):SetFloat3(X, 0.40, Z)
+		
 		
 		local comp = world:GetComponent(entities[i], "River", 0)
         local ix, iy, iz = comp:GetInt3(0)
 		
 		if ix == 1 then
-			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, math.pi, 0)
-		elseif iy ~= 0 then
-			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, iy*math.pi/2, 0)
-		else
+			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, -math.pi/2, 0)
+		elseif iy == 1 then
+			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, -iy*math.pi, 0)
+		elseif iy == -1 then
 			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, 0, 0)
+		else
+			world:GetComponent(newFlowingTile, "Rotation", "X"):SetFloat3(0, math.pi/2, 0)
 		end
 		
 	end
