cdb7b3be3f66db5be5dde8ce908f831e1482060a
Marcus Svensson <marcussvensson92@live.se>
2015-02-27 16:18:56 +0100

    Visualize Selected Cards bug fix

--- Externals/content/scripting/storaspelthreaded/Systems/Client/MiscSystems/cl_VisualizeSelectedCards.lua
+++ Externals/content/scripting/storaspelthreaded/Systems/Client/MiscSystems/cl_VisualizeSelectedCards.lua
@@ -81,7 +81,7 @@ VisualizeSelectedCards.EntitiesRemoved = function(self, dt, entities)
 	
 	local	nCards	=	self:GetEntities("CardSelected")
 	
-	if #nCards == 0 then
+	if #nCards == 0 and self.GhostEntity ~= -1 then
 		if not world:EntityHasComponent(self.GhostEntity, "Hide") then
 			world:CreateComponentAndAddTo("Hide", self.GhostEntity)
 		end
