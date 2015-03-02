749ad76b0838f2692eae2105df81d2afe4b58b12
Gabriella <Gabriella@stationar.datorsal>
2015-03-02 11:50:25 +0100

    nya buttons och checkbox - NE

--- Source/Game/Systems/AddTextToTextureSystem.cpp
+++ Source/Game/Systems/AddTextToTextureSystem.cpp
@@ -19,7 +19,6 @@ void AddTextToTextureSystem::Initialize()
 
 void AddTextToTextureSystem::PostInitialize()
 {
-	//m_graphics->AddFont("content/fonts/crayonkids.ttf", 72);
 	m_graphics->AddFont("content/fonts/barthowheel.ttf", 72);
 	m_graphics->AddFont("content/fonts/verdanab.ttf", 72);
 
