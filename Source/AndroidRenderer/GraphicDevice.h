9f95256888d47f442b248b22a6ef3df45493bf99
Pirat kaptenen <anders.kruger@hotmail.com>
2015-03-08 19:27:11 +0100

    prework for animations on android

--- Source/AndroidRenderer/GraphicDevice.h
+++ Source/AndroidRenderer/GraphicDevice.h
@@ -99,6 +99,13 @@ namespace Renderer
 		bool CastShadow;
 	};
 
+	struct ModelTexture
+	{
+		int id;
+		std::string textureName;
+		int textureType;
+	};
+
 	struct ParticleSystemToLoad
 	{
 		std::string Name;
@@ -121,156 +128,166 @@ namespace Renderer
 		GraphicDevice(Camera _camera);
 		virtual ~GraphicDevice();
 
+		// BASIC
 		virtual bool Init(){ return false; };
-
 		void PollEvent(SDL_Event _event);
 		virtual void Update(float _dt){};
 		virtual void Render(){};
 
+		// WINDOW
+		//SDL_Window*	GetSDL_Window(){ return m_window; } // IN PC GD
+		//SDL_GLContext GetSDL_GLContext(){ return m_glContext; } // IN PC GD
+		void GetWindowSize(int &x, int &y){ x = m_clientWidth; y = m_clientHeight; }
+		//void GetWindowPos(int &x, int &y); // IN PC GD
 		void ResizeWindow(int _width, int _height);
 		virtual void SetTitle(std::string _title){};
 
-		// SIMPLETEXT FROM GAME
+		// CAMERA
+		Camera *GetCamera(){ return m_camera; }
+
+		// MODEL
+		virtual bool PreLoadModel(std::vector<std::string> _dirs, std::string _file, int _renderType = RENDER_FORWARD){ return false; }; // TODO: not in GD in PC
+		virtual int LoadModel(std::vector<std::string> _dirs, std::string _file, glm::mat4 *_matrixPtr, int _renderType = RENDER_FORWARD, float* _color = nullptr, bool _castShadow = true, bool _isStatic = false){ return 0; }; // TODO: no virtual
+		int LoadModel(ModelToLoadFromSource* _modelToLoad);
+		virtual bool RemoveModel(int _id){ return false; }; // TODO: no virtual
+		virtual bool ActiveModel(int _id, bool _active){ return false; };  // TODO: no virtual
+
+		// ANIMATIONS
+		//glm::mat4 GetJointMatrix(int _modelId, int _jointId); // IN PC GD
+		//bool SetAnimation(int _modelId, int _animId, float _frameTime); // IN PC GD
+
+		// TEXTURES
+		virtual bool ChangeModelTexture(int _id, std::string _fileDir, int _textureType = TEXTURE_DIFFUSE){ m_modelTextures.push_back({ _id, _fileDir, _textureType }); return false; };
+		virtual bool ChangeModelNormalMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_NORMAL }); return false; };
+		virtual bool ChangeModelSpecularMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_SPECULAR }); return false; };
+
+		// LIGHTNING
+		virtual void BufferPointlights(int _nrOfLights, float **_lightPointers){};
+		virtual void BufferDirectionalLight(float *_lightPointer){};
+
+		// TEXT RENDERING
+		int AddFont(const std::string& filepath, int size);
+		float CreateTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, glm::ivec2 size = glm::ivec2(-1, -1));
+		void CreateWrappedTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, unsigned int wrapLength, glm::ivec2 size = glm::ivec2(-1, -1));
+		
+		// PARTICLES
+		void AddParticleEffect(std::string _name, const vec3 _pos, int _nParticles, float _lifeTime, float _scale, float _spriteSize, std::string _texture, vec3 _color, int &_id);
+		void RemoveParticleEffect(int _id);
+		void SetParticleAcceleration(int _id, float x, float y, float z);
+
+		// SIMPLETEXT
 		virtual bool RenderSimpleText(std::string _text, int x, int y){ return false; }
 		virtual void SetSimpleTextColor(float _r, float _g, float _b, float _a){};
 		virtual void SetDisco(){};
 		virtual void ToggleSimpleText(){};
 		virtual void ToggleSimpleText(bool _on){};
 
-		Camera *GetCamera(){ return m_camera; }
-		void GetWindowSize(int &x, int &y){ x = m_clientWidth; y = m_clientHeight; }
-
-		// ANIMATIONS
-		bool SetAnimation(int _modelId, int _animId, float _frameTime);
-		glm::mat4 GetJointMatrix(int _modelId, int _jointId);
-
-		// MODELLOADER
-		virtual bool PreLoadModel(std::vector<std::string> _dirs, std::string _file, int _renderType = RENDER_FORWARD){ return false; };
-		virtual int LoadModel(std::vector<std::string> _dirs, std::string _file, glm::mat4 *_matrixPtr, int _renderType = RENDER_FORWARD, float* _color = nullptr, bool _castShadow = true, bool _isStatic = false){ return 0; };
-		int LoadModel(ModelToLoadFromSource* _modelToLoad);
-		virtual bool RemoveModel(int _id){ return false; };
-		virtual bool ActiveModel(int _id, bool _active){ return false; };
-		virtual bool ChangeModelTexture(int _id, std::string _fileDir, int _textureType = TEXTURE_DIFFUSE){ m_modelTextures.push_back({ _id, _fileDir, _textureType }); return false; };
-		virtual bool ChangeModelNormalMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_NORMAL }); return false; };
-		virtual bool ChangeModelSpecularMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_SPECULAR }); return false; };
-
+		// ÖVRIGT
 		void SetDebugTexFlag(int _flag) { return; }
-		virtual void BufferPointlights(int _nrOfLights, float **_lightPointers){};
-		virtual void BufferDirectionalLight(float *_lightPointer){};
-
+		void Clear(); // virtual in PC
 		int GetVRamUsage(){ return -1; }
-		
-		void Clear();
-		
-		int AddFont(const std::string& filepath, int size);
-		float CreateTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, glm::ivec2 size = glm::ivec2(-1, -1));
-		void CreateWrappedTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, unsigned int wrapLength, glm::ivec2 size = glm::ivec2(-1, -1));
-
-		void AddParticleEffect(std::string _name, const vec3 _pos, int _nParticles, float _lifeTime, float _scale, float _spriteSize, std::string _texture, vec3 _color, int &_id);
-		void RemoveParticleEffect(int _id);
-		void SetParticleAcceleration(int _id, float x, float y, float z);
 
 	protected:
-		bool InitSkybox();
-		
-		virtual bool InitLightBuffers() = 0;
-
-		virtual void BufferLightsToGPU() = 0;
-
-		void InitFBO();
-		void InitStandardBuffers();
-		void InitStandardShaders();
-
-		Camera* m_camera;
-
-		SDL_Window*		m_window;
 		SDL_GLContext	m_glContext;
+		bool			m_SDLinitialized;
+		float			m_dt, m_elapsedTime;
+		int				m_fps;
 
-		// dt and fps
-		float m_dt;
-		int m_fps;
-
-		// For the river animation
-		float m_elapsedTime;
-
-		bool m_SDLinitialized;
-
-		// Window size
+		// Window
+		SDL_Window*		m_window;
 		int	m_clientWidth, m_clientHeight;
 
-		// Light info
+		// Camera
+		Camera* m_camera;
+
+		// Models
+		int m_modelIDcounter;
+		//bool m_useAnimations;
+		//std::vector<RenderList> m_renderLists;
+		std::vector<Model> m_modelsForward, m_modelsViewspace, m_modelsInterface, m_modelsWater, m_modelsWaterCorners;
+		//std::vector<AModel> m_modelsAnimated;
+		std::map<int, ModelToLoad*> m_modelsToLoad;
+		std::map<int, ModelToLoadFromSource*> m_modelsToLoadFromSource;
+		std::map<int, std::vector<ModelToLoad*>> m_staticModelsToLoad;
+
+		// Data
+		std::vector<ModelTexture> m_modelTextures;
+		std::map<const std::string, GLuint> m_textures;
+		std::map<const std::string, Buffer*> m_meshs;
+		std::vector<std::pair<std::string, SDL_Surface*>> m_surfaces;
+
+		// Skybox
+		SkyBox *m_skybox;
+
+		// Particles
+		int m_particleID;
+		std::map<int, ParticleEffect*> m_particleEffects;
+		std::vector<ParticleSystemToLoad> m_particleSystemsToLoad;
+		std::vector<int> m_particlesIdToRemove;
+
+		// Shaders
+		Shader m_fullscreen;
+		Shader m_skyBoxShader;
+		Shader m_forwardShader;
+		Shader m_viewspaceShader;
+		Shader m_interfaceShader;
+		//Shader m_shadowShaderForward;
+		Shader m_riverShader, m_riverCornerShader;
+		std::map<std::string, Shader*> m_particleShaders;
+
+		// Lights
 		vec3 m_dirLightDirection;
 		float m_lightDefaults[10];
 		float* m_directionalLightPtr;
 		float** m_pointlightsPtr;
 		int m_nrOfLightsToBuffer;
 
-		// Particles stuff
-		std::map<int, ParticleEffect*> m_particleEffects;
-		// For adding and removing on main thread
-		std::vector<ParticleSystemToLoad> m_particleSystemsToLoad;
-		std::vector<int> m_particlesIdToRemove;
-		int m_particleID;
 
+		// BASIC
+		//virtual void InitRenderLists() { return; }
+		void InitStandardBuffers();
+		void InitStandardShaders();
+		//bool InitSDLWindow(int _width = 1280, int _height = 720);
+		void InitFBO();
+		virtual bool InitLightBuffers() = 0;
+
+		// Models
+		virtual void BufferModels();	// TODO: no virtual
+		virtual void BufferModel(int _modelId, ModelToLoad* _modelToLoad);	// TODO: no virtual
+		void BufferModel(int _modelId, ModelToLoadFromSource* _modelToLoad);
+		//void BufferAModel(int _modelId, ModelToLoad* _modelToLoad);
+
+		// Data
+		virtual Buffer* AddMesh(std::string _fileDir, Shader *_shaderProg) = 0; // TODO: no virtual
+		virtual Buffer* AddMesh(ModelToLoadFromSource* _modelToLoad, Shader *_shaderProg) = 0; // TODO: no virtual
+		void SortModelsBasedOnDepth(std::vector<Model>* models);
+		void BufferStaticModel(std::pair<int, std::vector<ModelToLoad*>> _staticModel);
+
+		// Skybox
+		bool InitSkybox();
+		
+		// PARTICLES
 		void BufferParticleSystems();
 
+		// TEXTURES	
+		GLuint AddTexture(std::string _fileDir, GLenum _textureSlot);
+		void BufferModelTextures();
+		virtual bool BufferModelTexture(int _id, std::string _fileDir, int _textureType);
+		void BufferSurfaces();
+
+		// Lights
+		virtual void BufferLightsToGPU() = 0;
+		void BufferLightsToGPU_GD();
+		
 		// The Framebuffer
 		GLuint m_FBO;
 		int m_framebufferWidth, m_framebufferHeight;
 		// Framebuffer textures
 		GLuint m_depthBuf, m_outputImage;
 
-		// Shaders
-		Shader m_skyBoxShader;
-		Shader m_forwardShader, m_viewspaceShader, m_interfaceShader;
-		Shader m_riverShader, m_riverCornerShader;
-		Shader m_fullscreen;
-		std::map<std::string, Shader*> m_particleShaders;
-
-		// Skybox
-		SkyBox *m_skybox;
-
-		// Modelloader
-		int m_modelIDcounter;
-		std::vector<Model> m_modelsForward, m_modelsViewspace, m_modelsInterface, m_modelsWater, m_modelsWaterCorners;
-
-		// Meshs
-		std::map<const std::string, Buffer*> m_meshs;
-		virtual Buffer* AddMesh(std::string _fileDir, Shader *_shaderProg) = 0;
-		virtual Buffer* AddMesh(ModelToLoadFromSource* _modelToLoad, Shader *_shaderProg) = 0;
-		// Textures
-		std::map<const std::string, GLuint> m_textures;
-		GLuint AddTexture(std::string _fileDir, GLenum _textureSlot);
-		
-		
-		std::map<int, ModelToLoad*> m_modelsToLoad;
-		std::map<int, ModelToLoadFromSource*> m_modelsToLoadFromSource;
-		std::map<int, std::vector<ModelToLoad*>> m_staticModelsToLoad;
-		virtual void BufferModels();
-		virtual void BufferModel(int _modelId, ModelToLoad* _modelToLoad);
-		void BufferModel(int _modelId, ModelToLoadFromSource* _modelToLoad);
-		
-		std::vector<std::pair<std::string, SDL_Surface*>> m_surfaces;
-		void BufferSurfaces();
-		
-		struct ModelTexture
-		{
-			int id;
-			std::string textureName;
-			int textureType;
-		};
-		std::vector<ModelTexture> m_modelTextures;
-		void BufferModelTextures();
-		virtual bool BufferModelTexture(int _id, std::string _fileDir, int _textureType);
-		
-		void SortModelsBasedOnDepth(std::vector<Model>* models);
-
-		void BufferLightsToGPU_GD();
-
 		GLuint m_fullscreenQuadBuffer;
 		void CreateFullscreenQuad();
 
-		void BufferStaticModel(std::pair<int, std::vector<ModelToLoad*>> _staticModel);
 	};
 }
 
--- Source/Renderer/GraphicDevice.h
+++ Source/Renderer/GraphicDevice.h
@@ -114,116 +114,109 @@ namespace Renderer
 		GraphicDevice(Camera _camera, int x, int y);
 		virtual ~GraphicDevice();
 
+		// BASIC
 		virtual bool Init(){ return false; };// = 0;
-
 		void PollEvent(SDL_Event _event);
 		virtual void Update(float _dt){};// = 0;
 		virtual void Render(){};// = 0;
 
+		// WINDOW
+		SDL_Window*	GetSDL_Window(){ return m_window; }
+		SDL_GLContext GetSDL_GLContext(){ return m_glContext; }
+		void GetWindowSize(int &x, int &y){ x = m_clientWidth; y = m_clientHeight; }
+		void GetWindowPos(int &x, int &y);
 		void ResizeWindow(int _width, int _height);
 		virtual void SetTitle(std::string _title){};// = 0;
 
-		// SIMPLETEXT FROM GAME
-		virtual bool RenderSimpleText(std::string _text, int x, int y){ return false; };// = 0;
-		virtual void SetSimpleTextColor(float _r, float _g, float _b, float _a){};// = 0;
-		virtual void SetDisco(){};// = 0;
-		virtual void ToggleSimpleText(){};// = 0;
-		virtual void ToggleSimpleText(bool _on){};// = 0;
-
+		// CAMERA
 		Camera* GetCamera(){ return m_camera; }
 
-		SDL_Window*	GetSDL_Window(){ return m_window; }
-		SDL_GLContext GetSDL_GLContext(){ return m_glContext; }
-
-		void GetWindowSize(int &x, int &y){ x = m_clientWidth; y = m_clientHeight; }
-
-		void GetWindowPos(int &x, int &y);
-
-		// ANIMATIONS
-		bool SetAnimation(int _modelId, int _animId, float _frameTime);
-		glm::mat4 GetJointMatrix(int _modelId, int _jointId);
-
-		// MODELLOADER
+		// MODEL
 		int LoadModel(std::vector<std::string> _dirs, std::string _file, glm::mat4 *_matrixPtr, int _renderType = RENDER_DEFERRED, float* _color = nullptr, bool _castShadow = true, bool _isStatic = false);
 		int LoadModel(ModelToLoadFromSource* _modelToLoad);
 		bool RemoveModel(int _id);// = 0;
 		bool ActiveModel(int _id, bool _active);// = 0;
+
+		// ANIMATIONS
+		glm::mat4 GetJointMatrix(int _modelId, int _jointId);
+		bool SetAnimation(int _modelId, int _animId, float _frameTime);
+
+		// TEXTURES
 		virtual bool ChangeModelTexture(int _id, std::string _fileDir, int _textureType = TEXTURE_DIFFUSE){ m_modelTextures.push_back({ _id, _fileDir, _textureType }); return false; };// = 0;
 		virtual bool ChangeModelNormalMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_NORMAL }); return false; };// = 0;
 		virtual bool ChangeModelSpecularMap(int _id, std::string _fileDir){ m_modelTextures.push_back({ _id, _fileDir, TEXTURE_SPECULAR }); return false; };// = 0;
 
-		void SetDebugTexFlag(int _flag) { m_debugTexFlag = _flag; }
+		// LIGHTNING
 		virtual void BufferPointlights(int _nrOfLights, float **_lightPointers){};// = 0;
 		virtual void BufferDirectionalLight(float *_lightPointer){};// = 0;
 		
-		virtual void Clear(){};// = 0;
-
-		int GetVRamUsage(){ return m_vramUsage; }
-		
+		// TEXT RENDERING
 		int AddFont(const std::string& filepath, int size);
 		float CreateTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, glm::ivec2 size = glm::ivec2(-1, -1));
 		void CreateWrappedTextTexture(const std::string& textureName, const std::string& textString, int fontIndex, SDL_Color color, unsigned int wrapLength, glm::ivec2 size = glm::ivec2(-1, -1));
 
+		// PARTICLES
 		void AddParticleEffect(std::string _name, const vec3 _pos, int _nParticles, float _lifeTime, float _scale, float _spriteSize, std::string _texture, vec3 _color, int &_id);
 		void RemoveParticleEffect(int _id);
 		void SetParticleAcceleration(int _id, float x, float y, float z);
 		
+		// SIMPLETEXT
+		virtual bool RenderSimpleText(std::string _text, int x, int y){ return false; };// = 0;
+		virtual void SetSimpleTextColor(float _r, float _g, float _b, float _a){};// = 0;
+		virtual void SetDisco(){};// = 0;
+		virtual void ToggleSimpleText(){};// = 0;
+		virtual void ToggleSimpleText(bool _on){};// = 0
+
+		// ÖVRIGT
+		void SetDebugTexFlag(int _flag) { m_debugTexFlag = _flag; }
+		virtual void Clear(){};// = 0;
+		int GetVRamUsage(){ return m_vramUsage; }
+
 	protected:
-		virtual void InitRenderLists() { return; }
-		void InitStandardShaders();
-		void InitStandardBuffers();
-		bool InitSDLWindow(int _width = 1280, int _height = 720);
-		bool InitSkybox();
-		void BufferModels();
-		void BufferModel(int _modelId, ModelToLoad* _modelToLoad);
-		void BufferAModel(int _modelId, ModelToLoad* _modelToLoad);
-		void BufferModel(int _modelId, ModelToLoadFromSource* _modelToLoad);
-
-		std::vector<ModelTexture> m_modelTextures;
-		void BufferModelTextures();
-		bool BufferModelTexture(int _id, std::string _fileDir, int _textureType = TEXTURE_DIFFUSE);
-
-		void UpdateTextureIndex(GLuint newTexture, GLuint oldTexture);
-		// Meshs
-		std::map<const std::string, Buffer*> m_meshs;
-		Buffer* AddMesh(std::string _fileDir, Shader *_shaderProg, bool animated);
-		Buffer* AddMesh(ModelToLoadFromSource* _modelToLoad, Shader *_shaderProg);
-
-		//modellists
-		std::vector<RenderList> m_renderLists;
-
-		std::vector<Model> m_modelsForward, m_modelsViewspace, m_modelsInterface, m_modelsWater, m_modelsWaterCorners;
-		std::vector<AModel> m_modelsAnimated;
-		std::vector<Model*> m_shadowModelsForward, m_shadowModelsDeferred;
-
-		Shader m_animationShader;
-
-		bool m_useAnimations;
-
-		//MODEL LOADER
-		int m_modelIDcounter;
-
-		SDL_Window*		m_window;
 		SDL_GLContext	m_glContext;
+		bool			m_SDLinitialized;
+		int				m_vramUsage; //in bytes
+		float			m_dt, m_elapsedTime;
+		int				m_fps;
+		int				m_debugTexFlag;
 
-		Camera* m_camera;
-		int m_vramUsage; //in bytes
-
-		// For the river animation
-		float m_elapsedTime;
-
-		// dt and fps
-		float m_dt;
-		int m_fps;
-
-		const char*	m_windowCaption;
-		bool m_SDLinitialized;
-
-		//// Window size
+		// Window
+		SDL_Window*		m_window;
+		const char*		m_windowCaption;
 		int	m_clientWidth, m_clientHeight;
 		int m_windowPosX, m_windowPosY;
 
+		// Camera
+		Camera*			m_camera;
+
+		// Models
+		int m_modelIDcounter;
+		bool m_useAnimations;
+		std::vector<RenderList> m_renderLists;
+		std::vector<Model> m_modelsForward, m_modelsViewspace, m_modelsInterface, m_modelsWater, m_modelsWaterCorners;
+		std::vector<Model*> m_shadowModelsForward, m_shadowModelsDeferred;
+		std::vector<AModel> m_modelsAnimated;
+		std::map<int, ModelToLoad*>					m_modelsToLoad;
+		std::map<int, ModelToLoadFromSource*>		m_modelsToLoadFromSource;
+		std::map<int, std::vector<ModelToLoad*>>	m_staticModelsToLoad;
+
+		// Data
+		std::vector<ModelTexture> m_modelTextures;
+		std::map<const std::string, GLuint> m_textures;
+		std::map<const std::string, Buffer*> m_meshs;
+		std::vector<std::pair<std::string, SDL_Surface*>> m_surfaces;
+
+		// Skybox
+		SkyBox *m_skybox;
+
+		// Particles
+		int m_particleID;
+		std::map<int, ParticleSystem*> m_particleSystems;
+		std::vector<ParticleSystemToLoad> m_particleSystemsToLoad;
+		std::vector<int> m_particlesIdToRemove;
+
 		// Shaders
+		Shader m_animationShader;
 		Shader m_skyBoxShader;
 		Shader m_forwardShader;
 		Shader m_viewspaceShader;
@@ -232,36 +225,42 @@ namespace Renderer
 		Shader m_riverShader, m_riverCornerShader;
 		std::map<std::string, Shader> m_particleShaders;
 
-		// Skybox
-		SkyBox *m_skybox;
-
-		// Particles
-		std::map<int, ParticleSystem*> m_particleSystems;
-		int m_particleID;
-		std::vector<ParticleSystemToLoad> m_particleSystemsToLoad;
-		std::vector<int> m_particlesIdToRemove;
-
-		//// DEBUG variables ----
-		int m_debugTexFlag;
-		std::map<int, ModelToLoad*> m_modelsToLoad;
-		std::map<int, ModelToLoadFromSource*> m_modelsToLoadFromSource;
-		std::map<int, std::vector<ModelToLoad*>> m_staticModelsToLoad;
+		// Lights
 		float**	m_pointerToPointlights;
 		int		m_numberOfPointlights;
 		float*	m_pointerToDirectionalLights;
 		int		m_numberOfDirectionalLights;
 
-		GLuint AddTexture(std::string _fileDir, GLenum _textureSlot);
-		std::map<const std::string, GLuint> m_textures;
-		
-		std::vector<std::pair<std::string, SDL_Surface*>> m_surfaces;
-		void BufferSurfaces();
+		// BASIC
+		virtual void InitRenderLists() { return; }
+		void InitStandardShaders();
+		void InitStandardBuffers();
+		bool InitSDLWindow(int _width = 1280, int _height = 720);
 
+		// Models
+		void BufferModels();
+		void BufferModel(int _modelId, ModelToLoad* _modelToLoad);
+		void BufferModel(int _modelId, ModelToLoadFromSource* _modelToLoad);
+		void BufferAModel(int _modelId, ModelToLoad* _modelToLoad);
+
+		// Data
+		Buffer* AddMesh(std::string _fileDir, Shader *_shaderProg, bool animated);
+		Buffer* AddMesh(ModelToLoadFromSource* _modelToLoad, Shader *_shaderProg);
 		void SortModelsBasedOnDepth(std::vector<Model>* models);
+		void BufferStaticModel(std::pair<int, std::vector<ModelToLoad*>> _staticModel);
 
+		// Skybox
+		bool InitSkybox();
+
+		// PARTICLES
 		void BufferParticleSystems();
 
-		void BufferStaticModel(std::pair<int, std::vector<ModelToLoad*>> _staticModel);
+		// TEXTURES		
+		GLuint AddTexture(std::string _fileDir, GLenum _textureSlot);
+		void BufferModelTextures();
+		bool BufferModelTexture(int _id, std::string _fileDir, int _textureType = TEXTURE_DIFFUSE);
+		void UpdateTextureIndex(GLuint newTexture, GLuint oldTexture);
+		void BufferSurfaces();
 	};
 }
 
