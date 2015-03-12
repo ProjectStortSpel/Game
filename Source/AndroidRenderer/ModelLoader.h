015bea760534936424dde81b0019df6c092b4b38
Marcus Svensson <marcussvensson92@live.se>
2015-03-12 18:15:57 +0100

    Animations on Android

--- Source/AndroidRenderer/ModelLoader.h
+++ Source/AndroidRenderer/ModelLoader.h
@@ -4,17 +4,46 @@
 
 struct ObjectData
 {
+	bool animated;
 	std::string mesh;
 	std::string text;
 	std::string norm;
 	std::string spec;
+	std::string joints;
+	std::vector<std::string> anim;
 	ObjectData()
 	{
+		animated = false;
 		mesh = text = norm = spec = "";
 	}
 	ObjectData(std::string dir)
 	{
+		animated = false;
 		mesh = text = norm = spec = dir;
+	}
+};
+
+struct JointData
+{
+	glm::mat4 mat;
+	float parent;
+	JointData(glm::mat4 _mat, float _parent)
+	{
+		mat = _mat;
+		parent = _parent;
+	}
+};
+
+struct AnimData
+{
+	int frame;
+	int joint;
+	glm::mat4 mat;
+	AnimData(int _frame, int _joint, glm::mat4 _mat)
+	{
+		frame = _frame;
+		joint = _joint;
+		mat = _mat;
 	}
 };
 
@@ -78,7 +107,7 @@ public:
 		{
 			std::string fileDir = dirs[i];
 			fileDir.append(file);
-
+			
 			SDL_RWops* fileIn = SDL_RWFromFile(fileDir.c_str(), "r");
 			if (fileIn != NULL)
 			{
@@ -159,11 +188,150 @@ public:
 			{
 				objectdata.spec = filePath;
 			}
+			
+			
+			nextFileIndex = dataString.find('\n', prevFileIndex);
+			if (nextFileIndex != std::string::npos)
+			{
+				temp = dataString.substr(prevFileIndex, nextFileIndex - prevFileIndex);
+				temp.erase(std::remove(temp.begin(), temp.end(), 13), temp.end());
+				prevFileIndex = nextFileIndex + 1;
+				if (GetFilePath(_dirs, temp, &filePath))
+				{
+					objectdata.joints = filePath;
+				}
+			}
 
+			while (true)
+			{
+				nextFileIndex = dataString.find('\n', prevFileIndex);
+				if (nextFileIndex == std::string::npos)
+					break;
+				temp = dataString.substr(prevFileIndex, nextFileIndex - prevFileIndex);
+				temp.erase(std::remove(temp.begin(), temp.end(), 13), temp.end());
+				prevFileIndex = nextFileIndex + 1;
+				if (GetFilePath(_dirs, temp, &filePath))
+				{
+					objectdata.anim.push_back(filePath);
+				}
+				else
+					break;
+			}
+			
+			int isamesh = objectdata.mesh.find(".amesh");
+			if (isamesh > 0 && objectdata.joints != "" && objectdata.anim.size() > 0)
+			{
+				objectdata.animated = true;
+			}
 		}
 		
 
 		return objectdata;
+	}
+	
+	static std::vector<JointData> importJoints(std::string fileDir)
+	{
+		std::vector<JointData> jointlistTemp;
+
+		SDL_RWops* fileIn = SDL_RWFromFile(fileDir.c_str(), "r");
+		Sint64 length = SDL_RWseek(fileIn, 0, RW_SEEK_END);
+		SDL_RWseek(fileIn, 0, RW_SEEK_SET);
+		// Read data
+		char* data = new char[length];
+		SDL_RWread(fileIn, data, length, 1);
+		std::istringstream iss(data);
+		delete[] data;
+		
+		if (fileIn)
+		{
+			while (iss)
+			{
+				int parent;
+				float xx, xy, xz, xw;
+				float yx, yy, yz, yw;
+				float zx, zy, zz, zw;
+				float wx, wy, wz, ww;
+				
+				iss >> parent;
+				iss >> xx >> xy >> xz >> xw;
+				iss >> yx >> yy >> yz >> yw;
+				iss >> zx >> zy >> zz >> zw;
+				iss >> wx >> wy >> wz >> ww;
+				
+				glm::mat4 matrix = glm::mat4(xx, xy, xz, xw,
+					yx, yy, yz, yw,
+					zx, zy, zz, zw,
+					wx, wy, wz, ww);
+
+				jointlistTemp.push_back(JointData(matrix, parent));
+			}
+		}
+		jointlistTemp.pop_back(); // Da fuck?
+		
+		// Close file
+		SDL_RWclose(fileIn);
+
+		for (int i = 0; i < jointlistTemp.size(); i++)
+		{
+			if (jointlistTemp[i].parent >= 0)
+			{
+				jointlistTemp[i].mat = jointlistTemp[int(jointlistTemp[i].parent)].mat * jointlistTemp[i].mat;
+			}
+		}
+
+		for (int i = 0; i < jointlistTemp.size(); i++)
+		{
+			jointlistTemp[i].mat = glm::inverse(jointlistTemp[i].mat);
+		}
+
+		return jointlistTemp;
+	}
+
+	static std::vector<AnimData> importAnimation(std::string fileDir)
+	{
+		std::vector<AnimData> jointlistTemp;
+	
+		SDL_RWops* fileIn = SDL_RWFromFile(fileDir.c_str(), "r");
+		Sint64 length = SDL_RWseek(fileIn, 0, RW_SEEK_END);
+		SDL_RWseek(fileIn, 0, RW_SEEK_SET);
+		// Read data
+		char* data = new char[length];
+		SDL_RWread(fileIn, data, length, 1);
+		std::istringstream iss(data);
+		delete[] data;
+		
+		if (fileIn)
+		{
+			while (iss)
+			{
+				int joint;
+				int frame;
+				float xx, xy, xz, xw;
+				float yx, yy, yz, yw;
+				float zx, zy, zz, zw;
+				float wx, wy, wz, ww;
+	
+				iss >> joint;
+				iss >> frame;
+				iss >> xx >> xy >> xz >> xw;
+				iss >> yx >> yy >> yz >> yw;
+				iss >> zx >> zy >> zz >> zw;
+				iss >> wx >> wy >> wz >> ww;
+	
+				glm::mat4 matrix = glm::mat4(xx, xy, xz, xw,
+					yx, yy, yz, yw,
+					zx, zy, zz, zw,
+					wx, wy, wz, ww);
+
+				jointlistTemp.push_back(AnimData(frame, joint, matrix));
+			}
+		}
+		jointlistTemp.pop_back();
+		
+		// Close file
+		SDL_RWclose(fileIn);
+
+		return jointlistTemp;
 	}
 };
 
