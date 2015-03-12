6f567a7be13fbfb3937b62bcb088910b45735914
Chris <c.ljungberg90@gmail.com>
2015-03-12 13:23:43 +0100

    commit to pull

--- Source/Renderer/ParticleSystem.cpp
+++ Source/Renderer/ParticleSystem.cpp
@@ -18,7 +18,17 @@ ParticleSystem::ParticleSystem(std::string type, const vec3 _pos, const vec3 _ve
 
 	const char * outputNames[] = { "Position", "Velocity", "StartTime" };
 
-	if (type == "fire")
+	if (type == "explosion")
+	{
+		m_shader.InitShaderProgram();
+		m_shader.AddShader("content/shaders/particles/particleExplosionVS.glsl", GL_VERTEX_SHADER);
+		m_shader.AddShader("content/shaders/particles/particleExplosionFS.glsl", GL_FRAGMENT_SHADER);
+		glTransformFeedbackVaryings(m_shader.GetShaderProgram(), 3, outputNames, GL_SEPARATE_ATTRIBS);
+		m_shader.FinalizeShaderProgram();
+
+		CreateExplosion();
+	}
+	else if (type == "fire")
 	{
 		m_shader.InitShaderProgram();
 		m_shader.AddShader("content/shaders/particles/particleFireVS.glsl", GL_VERTEX_SHADER);
@@ -73,6 +83,137 @@ ParticleSystem::~ParticleSystem()
 	glDeleteTransformFeedbacks(2, m_feedback);
 	glDeleteBuffers(8, vboHandles);
 	glDeleteVertexArrays(2, m_particleArray);
+}
+
+void ParticleSystem::CreateExplosion()
+{
+	// Create and allocate buffers A and B for m_posBuf, m_velBuf and m_startTime
+	m_dstBlendFactor = GL_ONE_MINUS_SRC_ALPHA;
+	m_accel = vec3(0.0f * m_scale.x, 0.0f * m_scale.y, 0.0f * m_scale.z);
+	vec3 v(0.0f);
+	float velocity, theta, phi;
+	float mtime = 0.0f, rate = 0.0f; // (m_lifeTime / (float)m_nrParticles);//0.00075f;
+
+	GLfloat *posData = new GLfloat[m_nrParticles * 3];
+	GLfloat *velData = new GLfloat[m_nrParticles * 3];
+	GLfloat *timeData = new GLfloat[m_nrParticles];
+	GLfloat *initVelData;
+	GLfloat *initPosData;
+
+	srand(time(NULL));
+	for (GLuint i = 0; i < m_nrParticles; i++) {
+		posData[3 * i] = ((float)(rand() % 9) - 4)*0.5 * m_scale.x;
+		posData[3 * i + 1] = ((float)(rand() % 9) - 4)*0.5 * m_scale.y;
+		posData[3 * i + 2] = ((float)(rand() % 9 - 4))*0.5 * m_scale.z;
+
+		// Pick the direction of the velocity
+		theta = glm::mix(0.0f, (float)M_PI / 6.0f, (float)(rand() % 101) / 100);
+		phi = glm::mix(0.0f, (float)(2 * M_PI), (float)(rand() % 101) / 100);
+
+		//printf("rand val %f \n" , sin((float)(rand() % (int)(2*M_PI*1000))/1000));
+
+		v.x = sin((float)(rand() % (int)(2 * M_PI * 1000) + 1) / 1000);      //((float)(rand() % 101)/100 -0.5)*0.1;
+		v.y = sin((float)(rand() % (int)(2 * M_PI * 1000) + 1) / 1000);      //((float)(rand() % 101)/100 -0.5)*0.1;
+		//if (v.y > 0)
+		//	v.y *= 2;
+		v.z = sin((float)(rand() % (int)(2 * M_PI * 1000) + 1) / 1000);      //((float)(rand() % 101)/100 -0.5)*0.1; 
+
+		v = glm::normalize(v) * (((float)(rand() % 501)+1000.f) / 3000.f);
+
+		// Scale to set the magnitude of the velocity (speed)
+		velocity = glm::mix(1.25f, 1.5f, (float)(rand() % 101) / 100);
+		v = v * velocity;
+		//v *= scale;
+		velData[3 * i] = v.x;
+		velData[3 * i + 1] = v.y;
+		velData[3 * i + 2] = v.z;
+
+		timeData[i] = mtime;
+		mtime += rate;
+	}
+	initVelData = velData;
+	initPosData = posData;
+
+	GLuint vboHandles[8];
+	glGenBuffers(8, vboHandles);
+	m_posBuf[0] = vboHandles[0];
+	m_posBuf[1] = vboHandles[1];
+	m_velBuf[0] = vboHandles[2];
+	m_velBuf[1] = vboHandles[3];
+	m_startTime[0] = vboHandles[4];
+	m_startTime[1] = vboHandles[5];
+	m_initVelBuf = vboHandles[6];
+	m_initPosBuf = vboHandles[7];
+
+	//Create the two vertex arrays
+	glGenVertexArrays(2, m_particleArray);
+	for (int i = 0; i < 2; i++)
+	{
+		glBindVertexArray(m_particleArray[i]);
+
+		// enable "vertex attribute arrays"
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glEnableVertexAttribArray(2);
+		glEnableVertexAttribArray(3);
+		glEnableVertexAttribArray(4);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_posBuf[i]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_velBuf[i]);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_startTime[i]);
+		glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_initVelBuf);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_initPosBuf);
+		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+	}
+	glBindVertexArray(0);
+
+	//kanske ska vara glBufferData här ------   OBS!  --------
+	for (int i = 0; i < 2; i++)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_posBuf[i]);
+		glBufferData(GL_ARRAY_BUFFER, m_nrParticles * 3 * sizeof(float), posData, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_velBuf[i]);
+		glBufferData(GL_ARRAY_BUFFER, m_nrParticles * 3 * sizeof(float), velData, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_startTime[i]);
+		glBufferData(GL_ARRAY_BUFFER, m_nrParticles * sizeof(float), timeData, GL_DYNAMIC_DRAW);
+
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, m_initVelBuf);
+	glBufferData(GL_ARRAY_BUFFER, m_nrParticles * 3 * sizeof(float), initVelData, GL_DYNAMIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_initPosBuf);
+	glBufferData(GL_ARRAY_BUFFER, m_nrParticles * 3 * sizeof(float), initPosData, GL_DYNAMIC_DRAW);
+
+	// Setup the feedback objects
+	glGenTransformFeedbacks(2, m_feedback);
+
+	// Transform feedback 0
+	glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, m_feedback[0]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, m_posBuf[0]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 1, m_velBuf[0]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 2, m_startTime[0]);
+
+	// Transform feedback 1
+	glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, m_feedback[1]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, m_posBuf[1]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 1, m_velBuf[1]);
+	glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 2, m_startTime[1]);
+
+	delete[] posData;
+	delete[] velData;
+	delete[] timeData;
+	initVelData = 0;
+	initPosData = 0;
 }
 
 void ParticleSystem::CreateFire()
