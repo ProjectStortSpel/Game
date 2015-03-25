565cb04e7e32f0d53421e77a57e21244e73f78b4
unknown <c.ljungberg90@gmail.com>
2015-03-25 20:27:09 +0100

    Peter panning fix

--- Source/Renderer/ShadowMap.cpp
+++ Source/Renderer/ShadowMap.cpp
@@ -7,7 +7,7 @@ ShadowMap::ShadowMap(vec3 lightPos, vec3 target, int res)
 	m_targetDirection = target;
 
 	m_resolution = res;
-	m_projectionMatrix = glm::ortho(-10.0, 10.0, -10.0, 10.0, 1.5, 100.0); 
+	m_projectionMatrix = glm::ortho(-10.0, 10.0, -10.0, 10.0, 1.5, 20.0); 
 	//glm::perspective(45.0f, (float)res / (float)res, 1.0f, 50.0f);
 	m_viewMatrix = glm::lookAt(m_lightPosition, m_targetDirection, vec3(0.0f, 1.0f, 0.0f));
 	float val = 0.5f;
@@ -74,5 +74,5 @@ void ShadowMap::SetBounds(float _width, float _height)
 {
 	m_width = _width;
 	m_height = _height;
-	m_projectionMatrix = glm::ortho(-_width, _width, -_height, _height, 1.5f, 100.f + std::max(_width, _height));
+	m_projectionMatrix = glm::ortho(-_width, _width, -_height, _height, 1.5f, 20.f + std::max(_width, _height));
 }
